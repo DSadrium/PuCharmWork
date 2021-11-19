ad3423b6bd4505127088ce9f5cec0de810e80fb4
DSadrium <argamak-94@mail.ru>
2021-11-19 16:13:00 +0500

    Point 1 to 4

---
+++ filter.py
@@ -0,0 +1,33 @@
+import numpy as np
+from PIL import Image
+
+
+def get_img(path):
+    img = Image.open(path)
+    return np.array(img)
+
+
+def save_img(img, name):
+    res = Image.fromarray(img)
+    res.save(name)
+
+
+def get_pixel_art(img, size, grayscale):
+    width = len(img)
+    height = len(img[1])
+
+    for x in range(0, width, size):
+        for y in range(0, height, size):
+            brightness = get_brightness(img, size, x, y)
+            img[x: x + size, y: y + size] = brightness - brightness % grayscale
+    return img
+
+
+def get_brightness(img, size, x, y):
+    return np.average(img[x: x + size, y: y + size])
+
+
+img = get_img(input("Write the picture's path - "))
+size = int(input("Write pixel's size - "))
+grayscale = int(input("Write grayscale's count - "))
+save_img(get_pixel_art(img, size, grayscale), 'res.jpg')
